1787245d3a08bd04fc6487da2e0c67c774688645
Anish Dalal <anishdalal1@gmail.com>
2022-12-08 18:02:19 -0500

    add hovering goodies

--- pages/draft.js
+++ pages/draft.js
@@ -4,6 +4,7 @@ import { terms } from '../data/term_sheet'
 
 export default function Draft() {
 
+  const [activeTerm, setActiveTerm] = useState("")
   const [data, setData] = useState({})
   useEffect(() => {
     setData(terms)
@@ -34,9 +35,9 @@ export default function Draft() {
 
       <div className="mt-6 flex max-w-4xl flex-wrap justify-around sm:w-full">
         <div className="mt-6 w-6/12 rounded-xl border p-6 text-left">
-          <h3 className="text-2xl font-bold">Terms</h3>
+          <h3 className="text-2xl pl-2 font-bold">Terms</h3>
           {Object.keys(data).map((term) =>
-            <p className="mt-4 text-xl">
+            <p key={term} onClick={(e) => setActiveTerm(term)} className={`mt-4 p-2 text-xl rounded-md ${activeTerm === term ? "bg-slate-50" : "hover:bg-slate-100"} ${activeTerm !== term && activeTerm !== "" ? "opacity-40" : ""}`}>
               <span className="font-bold">{data[term].display_name}</span>:
               <input
                 className="shadow appearance-none border rounded-lg w-1/2 text-lg py-1 ml-2 px-3 text-gray-700 bg-gray-50 leading-tight focus:outline-none focus:shadow-outline"
@@ -46,8 +47,11 @@ export default function Draft() {
                 onChange={handleChange}>
               </input>
               <br />
-              {data[term].text &&
-                <span className="text-base">{data[term].text.substring(0, 100)}...</span>
+              {data[term].text && term !== activeTerm && 
+                <span className="text-base">{data[term].text.split(" ", 10).join(" ")}...</span>
+              }
+              {data[term].text && term === activeTerm &&
+                <span className="text-base">{data[term].text}</span>
               }
             </p>
           )}
